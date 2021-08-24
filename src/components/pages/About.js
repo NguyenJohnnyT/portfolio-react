4ba02a0465a12c6ac482b2405548a56faf68d6e5
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-23 22:53:02 -0700

    fix images, add style to src

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./style.css"
 function About () {
   return (
-    <div class="jumbotron jumbotron-fluid p-4 text-light d-flex align-items-center">
-      <h1 class="display-4">Johnny Nguyen</h1>
+    <div className="jumbotron jumbotron-fluid p-4 text-light d-flex align-items-center">
+      <h1 className="display-4">Johnny Nguyen</h1>
         <p>Based in Santa Clara, California, I am an aspiring programmer with practice in various languages including HTML, CSS, JavaScript, and Python</p>
     </div>
   )
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,47 +1,47 @@
 import React from "react";
-
+import "./style.css"
 function Home() {
   let styles = {
     header: {
-      "margin-right": "0"
+      "marginRight": "0"
     },
     text_deco: {
-      "text-decoration": "none"
+      "textDecoration": "none"
     }
   }
 
   return (
-    <div class="row d-flex justify-content-center" style={styles.header}>
+    <div className="row d-flex justify-content-center" style={styles.header}>
 
-      <div class="accordion m-3" id="accordionExample">
+      <div className="accordion m-3" id="accordionExample">
 
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingOne">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">About Me:</button>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">About Me:</button>
           </h2>
-          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-            <div class="accordion-body row">
-              <div class = "col-4 accord-img-content">
-                <img src="./assets/images/johnnypfp.jfif" class='img-accord m-2' alt="pfp" />
+          <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div className="accordion-body row">
+              <div className = "col-4 accord-img-content">
+                <img src="./images/johnnypfp.jfif" className='img-accord m-2' alt="pfp" />
               </div>
-              <div class="col-8 accord-content pt-2"><strong>Hello!</strong> Welcome to my website!  A soon to be graduate of the UC Berkeley Extension Coding Boot Camp, I have aspirations to becoming a web developer with a heavy interest in the backend routes.  I love music, fitness, teaching and connecting with people of similar mind.  Feel free to check out any of the projects and freelance projects I have done over the past year.  
+              <div className="col-8 accord-content pt-2"><strong>Hello!</strong> Welcome to my website!  A soon to be graduate of the UC Berkeley Extension Coding Boot Camp, I have aspirations to becoming a web developer with a heavy interest in the backend routes.  I love music, fitness, teaching and connecting with people of similar mind.  Feel free to check out any of the projects and freelance projects I have done over the past year.  
               </div>
             </div>
           </div>
         </div>
 
-        <div class="accordion-item" >
-          <h2 class="accordion-header" id="headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <div className="accordion-item" >
+          <h2 className="accordion-header" id="headingTwo">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
               Skills:
             </button>
           </h2>
-          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-            <div class="accordion-body row">
-              <div class = "col-4 accord-img-content">
-                <img src="./assets/images/Sample repo images/TWHlogo.png" class='img-accord m-2' alt="TWH" />
+          <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div className="accordion-body row">
+              <div className = "col-4 accord-img-content">
+                <img src="./images/TWHlogo.png" className='img-accord m-2' alt="TWH" />
               </div>
-                <div class="col-8 accord-content pt-2"><strong>Looking for a particular skillset?  These are the technologies I work with and have great interest in:</strong> 
+                <div className="col-8 accord-content pt-2"><strong>Looking for a particular skillset?  These are the technologies I work with and have great interest in:</strong> 
                   <ul> 
                     <li>HTML5, CSS, Javascript, Python</li> 
                     <li>Bootstrap, Handlebars, jQuery, Insomnia, Sequelize, MySQL, HowlerJS, Heroku, bcrypt, sessions, express.js, node.js</li>  
@@ -54,18 +54,18 @@ function Home() {
           </div>
         </div>
 
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingThree">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
               Projects and experience:
             </button>
           </h2>
-          <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-            <div class="accordion-body row">
-              <div class = "col-4 accord-img-content">
-                <img src="./assets/images/eeveeBig.png" class='img-accord m-2' alt="eevee" />
+          <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+            <div className="accordion-body row">
+              <div className = "col-4 accord-img-content">
+                <img src="./images/eeveeBig.png" className='img-accord m-2' alt="eevee" />
               </div>
-                <div class="col-8 accord-content pt-2"><strong>For a further exploration of my projects, check out my <a href="https://github.com/NguyenJohnnyT" style={styles.text_deco}>gitHub!</a></strong>
+                <div className="col-8 accord-content pt-2"><strong>For a further exploration of my projects, check out my <a href="https://github.com/NguyenJohnnyT" style={styles.text_deco}>gitHub!</a></strong>
                   <ul> 
                     <li>Full Stack Focused: Employee Management System, Three Word History, Tech Blog, Pokebattler</li> 
                     <li>Node and/or express: Team Profile generator, Note Taker, E-Commerce, README Generator</li>
