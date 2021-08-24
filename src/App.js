ecb65f0abad3672562909a78e2217afa86857264
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-23 21:48:25 -0700

    add page files, dependencies

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/Nav/NavTabs";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Projects from "./components/pages/Projects";
+
+function App() {
+  return null
+}
