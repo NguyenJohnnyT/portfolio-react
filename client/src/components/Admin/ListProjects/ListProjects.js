3d6c206153fc7d676a1303ef7ea73b2d529f24bf
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-09-14 14:18:34 -0700

    add admin

---
+++ client/src/components/Admin/ListProjects/ListProjects.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+
+function ListProjects () {
+  return 
+}
+
+export default ListProjects
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,8 +1,30 @@
-import React, { useState } from "react";
-import "./style.css"
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import ListProjects from "../components/Admin/ListProjects/ListProjects";
+import AdminForm from "../components/Admin/AdminForm/AdminForm";
+import getProjects from "../utils/API";
 
-function Admin () {
-  return
+const Admin = () => {
+  const [ project, setProject ] = useState([]);
+
+  useEffect(() => {
+    const asyncGetProjects = async () => {
+      let results = await getProjects();
+      setProject(results)
+    };
+    asyncGetProjects();
+  }, [project])
+
+  return (
+    <>
+      <div className='row'>
+        <ListProjects className='col' projectList={project} />
+      </div>
+      <div className='row'>
+        <AdminForm className='col'/>
+      </div>
+    </>
+  )
 }
 
 export default Admin;
