eba6a12e0fbb61522ab3d7c1fae64761cd1d41b2
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-09-16 10:47:01 -0700

    add new skilles to seed

--- server/seeders/skillSeed.js
+++ server/seeders/skillSeed.js
@@ -5,11 +5,90 @@ const skillData = [
     name: "JavaScript"
   },
   {
-    name: "HTML"
+    name: "HTML5"
   },
   {
     name: "CSS"
-  }
+  },
+  {
+    name: "Web API"
+  },
+  {
+    name: "jQuery"
+  },
+  {
+    name: "Server Side API"
+  },
+  {
+    name: "Insomnia"
+  },
+  {
+    name: "Fetch"
+  },
+  {
+    name: "JSON"
+  },
+  {
+    name: "Node.js"
+  },
+  {
+    name: "Express.js"
+  },
+  {
+    name: "Handlebars.js"
+  },
+  {
+    name: "Heroku"
+  },
+  {
+    name: "JSON Web Tokens"
+  },
+  {
+    name: "MySQL"
+  },
+  {
+    name: "Sequelize ORM"
+  },
+  {
+    name: "MongoDB"
+  },
+  {
+    name: "Mongoose ODM"
+  },
+  {
+    name: "Object-Oriented Programming"
+  },
+  {
+    name: "React"
+  },
+  {
+    name: "React Hooks"
+  },
+  {
+    name: "JSX"
+  },
+  {
+    name: "GraphQL"
+  },
+  {
+    name: "bcrypt"
+  },
+  {
+    name: "dotenv"
+  },
+  {
+    name: "node-cron"
+  },
+  {
+    name: "node-fetch"
+  },
+  {
+    name: "sweetalert2"
+  },
+  {
+    name: "howler"
+  },
+  
 ];
 
 const seedSkill = () => Skill.bulkCreate(skillData);
