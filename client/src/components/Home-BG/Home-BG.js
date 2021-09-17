29269f29fec4720af56d56f1dc865aeca71afceb
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-09-17 14:58:42 -0700

    add background placeholder, prep About for bg

--- client/src/components/Home-BG/Home-BG.js
+++ client/src/components/Home-BG/Home-BG.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 import { unsplashAPI } from '../../utils/unsplashAPI';
 import HomeText from './HomeText';
+import placeholder from '../../assets/img/placeholder/placeholderBG.png'
 
 const HomeBG = () => {
   const [background, setBackground] = useState([])
   
-  useEffect( ()=> {
-    const getBG = async () => {
-      let url = await unsplashAPI();
-      setBackground(url);
-    };
-    getBG();
-  }, [])
+  // useEffect( ()=> {
+  //   const getBG = async () => {
+  //     let url = await unsplashAPI();
+  //     setBackground(url);
+  //   };
+  //   getBG();
+  // }, [])
   
   const styles = {
     backgroundURL: {
@@ -21,16 +22,16 @@ const HomeBG = () => {
       backgroundSize: "cover"
     },
     noURL: {
-      backgroundImage: `url("https://mdbootstrap.com/img/new/standard/city/041.jpg")`,
+      backgroundImage: `url("${placeholder}")`,
       backgroundRepeat: 'no-repeat',
       backgroundSize: "cover"
     }
   }
   return(
     <>
-    {background 
+    {background.length > 1 
     ? <HomeText textStyle={styles.backgroundURL} />
-    : <HomeText textStyle={styles.noURL} />
+    : <HomeText textStyle={styles.noURL}/>
     }
     </>
   )
