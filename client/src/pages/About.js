66067eff0353c137778e2f2a930eb406b30fce8b
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-09-16 21:53:18 -0700

    add pfp, about styling

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./style.css"
+import me from "../assets/img/me/me.png"
 function About () {
   return (
     <div className="jumbotron jumbotron-fluid p-4 text-light d-flex align-items-center fillpage">
+      <div className='col-4 d-flex flex-column align-items-center'>
       <h1 className="display-4 m-5">Johnny Nguyen</h1>
+      <img src={me} className='pfp' alt="johnny"/>
+      </div>
       <div className='col-8'>
         <p>Based in Santa Clara, California, I am an aspiring programmer with practice in various languages including HTML, CSS, JavaScript, and Python</p>
         <br/>
@@ -12,6 +16,16 @@ function About () {
         <p>The Projects page is populated from what is stored in the <code>johnny-portfolio</code> database. The actual screenshots are imported from my Google Drive. For a look into the actual code and packages used, please visit 
           <a className='noLink' href="https://github.com/NguyenJohnnyT/portfolio-react"> the portfolio repo</a>
         .
+        </p>
+        <br/>
+        <p>
+          Lastly, an ever growing list of skills and tech I've covered inside and outside of UCBerkeley Bootcamp:
+          <ul>
+            <li><strong className="strongTitle">Languages: </strong> HTML, CSS, JavaScript, Python</li>
+            <li><strong className="strongTitle">Technologies: </strong> Bootstrap, jQuery, Git, Insomnia, indexDB, MongoDB, MySQL, Node.js, Express.js, Handlebars, Express-sessions, JSON Web Tokens, React, ReactHooks, GraphQL, Heroku, Insomnia</li>
+            <li><strong className="strongTitle">Concepts: </strong> JSON, Web, Server-side, & REST APIs, AJAX/Fetch, ES6,  Object-oriented Programming (OOP), Pseudocode, Data Structures, Algorithms, Front/Backend & Full Stack Development, Imperative & Declarative Programming</li>
+            <li><strong className="strongTitle">Packages of interest:  </strong> HowlerJS, node-cron, node-fetch, bcrypt, Sequelize, Mongoose, Morgan</li>
+          </ul>
         </p>
       </div>
     </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,8 @@
 import React from "react";
 import "./style.css"
 import HomeBG from "../components/Home-BG/Home-BG";
-import eevee from "../assets/img/misc/eeveeBig.png"
-import twhlogo from "../assets/img/projects/TWHlogo.png"
-import johnnypfp from "../assets/img/misc/johnnypfp.jfif"
 
 function Home() {
-  let styles = {
-    text_deco: {
-      "textDecoration": "none",
-      "color": "gold"
-    }
-  }
-
   return (
     <HomeBG />
   );
