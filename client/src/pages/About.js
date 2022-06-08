c987a1d42291b3f88cd508afc353a0a4e945defa
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-08 16:19:37 -0700

    change about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -11,7 +11,7 @@ function About () {
       </div>
       <div className='col-8 about-text'>
         <h3>Current Position:</h3>
-        <p>I am currently a front-end engineer for <a className='noLink' href="https://www.kernel.com">Kernel</a> and also a part time teaching assistant for <a className='noLink' href="https://bootcamp.berkeley.edu/coding/">UC Berkeley Extension Coding Boot Camp</a>.</p>  
+        <p>I am formerly a front-end engineer for <a className='noLink' href="https://www.kernel.com">Kernel</a> and currently a part time teaching assistant for <a className='noLink' href="https://bootcamp.berkeley.edu/coding/">UC Berkeley Extension Coding Boot Camp</a>.</p>  
         <br/>
         <h3>My Story:</h3>
         <p>I was born in the Bay Area where my journey took me through several career prospects--I graduated at San Jose State University with a chemistry degree before moving on to pharmacy school in Albany, NY. I graduated and became a licensed pharmacist in Nevada and Oregon. While I enjoyed interacting and helping patients through my internships, I felt a strong passion to build something in tech. Perhaps it was my roots in Silicon Valley calling me? In 2021, I dabbled in coding and eventually entered a program at UC Berkeley Extension Boot Camp for Web Development. I graduated and continue to build projects that inspire me. I love to teach others and entertain!</p>
@@ -24,13 +24,13 @@ function About () {
         <h3>My Skills and Technologies:</h3>
         <p>
           <ul>
-            <li><strong className="strongTitle">Proficient Languages: </strong> HTML, CSS, JavaScript, Python, SQL</li>
-            <li><strong className="strongTitle">Technologies and applications: </strong> Bootstrap, jQuery, Git, Insomnia, indexDB, MongoDB, MySQL, Node.js, Express.js, Handlebars, Express-sessions, JSON Web Tokens, React, ReactHooks, GraphQL, Heroku, Insomnia</li>
+            <li><strong className="strongTitle">Proficient Languages: </strong> HTML, SCSS, TypeScript, Python, SQL</li>
+            <li><strong className="strongTitle">Technologies and applications: </strong> Figma, Bootstrap, jQuery, Git, Insomnia, indexDB, MongoDB, MySQL, Node.js, Express.js, Handlebars, Express-sessions, JSON Web Tokens, React, ReactHooks, GraphQL, Heroku, Insomnia</li>
             <li><strong className="strongTitle">Learned Concepts: </strong> JSON, Web, Server-side, & REST APIs, AJAX/Fetch, ES6,  Object-oriented Programming (OOP), Pseudocode, Data Structures, Algorithms, Front/Backend & Full Stack Development, Imperative & Declarative Programming, deployment into the cloud (AWS, SAP)</li>
-            <li><strong className="strongTitle">Packages that I have worked with:  </strong> HowlerJS, node-cron, node-fetch, bcrypt, Sequelize, Mongoose, Morgan, react-phys-color, discord-py, discord-py-slash-command, dotenv</li>
+            <li><strong className="strongTitle">Packages that I have worked with:  </strong> Ant Design Components, React-Query, HowlerJS, node-cron, node-fetch, bcrypt, Sequelize, Mongoose, Morgan, react-phys-color, discord-py, discord-py-slash-command, dotenv</li>
           </ul>
         </p>
-        <p><sup>This page was last updated 1/28/2022</sup></p>
+        <p><sup>This page was last updated 6/08/2022</sup></p>
       </div>
     </div>
   )
