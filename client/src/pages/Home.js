8836b9a26805e3c9b48c7ba28a79984767ade1d6
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-20 11:32:51 -0700

    add homepage modal

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./style.css"
 import HomeBG from "../components/Home-BG/Home-BG";
+import Modal from "../components/Modal";
 
 function Home() {
   return (
+    <>
     <HomeBG />
+      <Modal />
+    </>
   );
 }
 
