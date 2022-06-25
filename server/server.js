7ae8884a73f715375c203c84a266063bbd2d9a64
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-25 10:49:09 -0700

    stop server running

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const cron = require('node-cron')
-const fetch = require('node-fetch')
+// const cron = require('node-cron')
+// const fetch = require('node-fetch')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,12 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes); 
 
-cron.schedule('0,2,10,20,30,40,50 * * * *', () => {
-  if (process.env.NODE_ENV === 'production') {
-    console.log('Keep server from sleeping every ten minutes.')
-    return fetch('https://jtn-portfolio.herokuapp.com/api/projects').then(res => res.json())
-  }
-})
+// cron.schedule('0,2,10,20,30,40,50 * * * *', () => {
+//   if (process.env.NODE_ENV === 'production') {
+//     console.log('Keep server from sleeping every ten minutes.')
+//     return fetch('https://jtn-portfolio.herokuapp.com/api/projects').then(res => res.json())
+//   }
+// })
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
